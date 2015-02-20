aab24da340e98e819f8a71970862f586482b3101
Muhammad Hijazi <irekasoft@gmail.com>
2015-02-21 00:01:33 +0800

    Initial Commit

---
+++ CoreData_iCloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreData_iCloud
+//
+//  Created by Hijazi on 21/2/15.
+//  Copyright (c) 2015 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreData_iCloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData_iCloud
+//
+//  Created by Hijazi on 21/2/15.
+//  Copyright (c) 2015 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
